00fd300f5b4021ff10533020c5d54267fce3f9c8
BWedeen <bewe0593@colorado.edu>
2022-03-12 17:39:55 -0700

    Frontend  Structure

--- CollatzFrontend/src/App.js
+++ CollatzFrontend/src/App.js
@@ -1,25 +1,49 @@
-import React, { useState, useRef } from 'react';
-import LocationSearch from './LocationSearch'
+// import React, { useState, useRef } from 'react';
+// import LocationSearch from './LocationSearch'
 
-function App() {
+// function App() {
 
-  const [userSearch, setUserSearch] = useState([])
-  const inputLocation = useRef()
+//   const [userSearch, setUserSearch] = useState([])
+//   const inputLocation = useRef()
 
-  function handleSearch() {
+//   function handleSearch() {
 
-  }
+//   }
 
+//   return (
+//     <>
+//         <LocationSearch />
+//         <input desiredLocation={inputLocation} type="text" />
+//         <button onClick={handleSearch}> Search </button>
+//         <button> Clear </button>
+
+//     </>
+//   );
+
+// }
+
+// export default App;
+
+import React from 'react'
+
+import { Footer, Possibility, Features, WhatCollatz, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+const App = () => {
   return (
-    <>
-        <LocationSearch />
-        <input desiredLocation={inputLocation} type="text" />
-        <button onClick={handleSearch}> Search </button>
-        <button> Clear </button>
-
-    </>
-  );
-
+    <div className = "App">
+      <div className = "gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatCollatz />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Footer />
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ CollatzFrontend/src/containers/whatCollatz/WhatCollatz.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './whatCollatz.css';
+
+const WhatCollatz = () => {
+  return (
+    <div>
+        WhatCollatz
+    </div>
+  )
+}
+
+export default WhatCollatz
