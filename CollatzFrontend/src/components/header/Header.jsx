e394287008bf15982fd68bdadab77623fbada144
BWedeen <bewe0593@colorado.edu>
2022-03-13 23:19:31 -0600

    Slider changes

--- CollatzFrontend/src/components/header/Header.jsx
+++ CollatzFrontend/src/components/header/Header.jsx
@@ -1,20 +1,52 @@
 import React from 'react';
 import './header.css';
 import lowPolyEarth from '../../assets/lowPolyEarth.png';
-import { Slider } from '@mui/material';
+import { Slider, Typography, Grid } from '@mui/material';
 
 const Header = () => {
+  const [value, setValue] = React.useState(50);
+
+  function calculateValue(value) {
+    return  value;
+  }
+
+  function valueLabelFormat(value) {
+    const units = ['$'];
+    let unitIndex = 0;
+  
+    return `${units[unitIndex]} ${value} `;
+  }
+
+  const handleChange = (event, newValue) => {
+    if (typeof newValue === 'number') {
+      setValue(newValue);
+    }
+  };
+
   return (
     <div className="collatz__header section__padding" id="home">
         <div className="collatz__header-content">
 
           <h1 className="gradient__text">Go Somewhere</h1>
           <p>Instantly plan trips within your budget. </p>
-          <p></p>
-
-          <Slider size="large" defaultValue={60} sx={{color: 'lightblue',}} 
-          aria-label="Default" valueLabelDisplay="auto" />
-
+          <div className="collatz__header-slider">
+            <Typography id="budget-slider" >
+                Budget: {valueLabelFormat(calculateValue(value))}
+            </Typography>
+            <Slider
+              size='medium'
+              value={value}
+              min={5}
+              step={1}
+              max={10000}
+              color = 'primary'
+              
+              getAriaValueText={valueLabelFormat}
+              valueLabelFormat={valueLabelFormat}
+              onChange={handleChange}
+              aria-labelledby="budget-slider"
+            />
+          </div>
           <div className="collatz__header-content_input">
             <input type="Place" placeholder="Your Destination"/>
             <button type="button"> Go </button>
