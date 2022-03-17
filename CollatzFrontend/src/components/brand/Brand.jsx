eb7608e292b10ed4c31a07c78a352f80ff3de97e
BWedeen <bewe0593@colorado.edu>
2022-03-16 20:21:56 -0600

    Lower icons

--- CollatzFrontend/src/components/brand/Brand.jsx
+++ CollatzFrontend/src/components/brand/Brand.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { airplane, hotel, attraction, car } from './import';
 import './brand.css';
 
 const Brand = () => {
   return (
-    <div>
-        Brand
+    <div className='collatz__brand section__padding'>
+      <div>
+        <img src={airplane} alt="airplane icon"/>
+      </div>
+      <div>
+        <img src={hotel} alt="hotel icon"/>
+      </div>
+      <div>
+      <img src={attraction} alt="attraction icon"/>
+      </div>  
+      <div>
+        <img src={car} alt="car icon"/>
+      </div>
     </div>
   )
 }
--- CollatzFrontend/src/components/header/Header.jsx
+++ CollatzFrontend/src/components/header/Header.jsx
@@ -4,18 +4,17 @@ import lowPolyEarth from '../../assets/lowPolyEarth.png';
 import { Slider, Typography, Grid } from '@mui/material';
 
 const Header = () => {
-  const [value, setValue] = React.useState(4321);
+  const [value, setValue] = React.useState(5000);
 
   function calculateValue(value) {
     return  value;
-  }
+  };
 
   function valueLabelFormat(value) {
     const units = ['$'];
     let unitIndex = 0;
-  
     return `${units[unitIndex]} ${value} `;
-  }
+  };
 
   const handleChange = (event, newValue) => {
     if (typeof newValue === 'number') {
@@ -26,7 +25,6 @@ const Header = () => {
   return (
     <div className="collatz__header section__padding" id="home">
         <div className="collatz__header-content">
-
           <h1 className="gradient__text">Go Somewhere New</h1>
           <p>Instantly plan trips within a set budget. </p>
           <div className="collatz__header-slider">
@@ -50,16 +48,10 @@ const Header = () => {
             <input type="Place" placeholder="Your Destination"/>
             <button type="button"> Go </button>
           </div>
-
-          
-         
-
         </div> 
-
         <div className="collatz__header-image">
           <img src={lowPolyEarth} alt="lowPolyEarth" />
         </div>
-
     </div>
   )
 }
