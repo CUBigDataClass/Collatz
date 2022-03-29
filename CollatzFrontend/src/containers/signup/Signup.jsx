b9d46cc0b003c66440081e8d6d4342d64482189d
qima53222 <1723162730@qq.com>
2022-03-28 22:40:46 -0600

    Login/out with Google account

--- CollatzFrontend/src/containers/signup/Signup.jsx
+++ CollatzFrontend/src/containers/signup/Signup.jsx
@@ -25,31 +25,3 @@ function Signup() {
 export default Signup;
 
 
-// const Signup = () => {
-//     return (
-//         <div className="Signup">
-//             <div className="container">
-//                 <h2>Welcome to Collatz New Friend!</h2>
-//                 <div className="input-box">
-//                     <label >Email</label>
-//                     <input type = "text"></input>
-//                 </div>
-//                 <div className="input-box">
-//                     <label >Password</label>
-//                     <input type = "password"></input>
-//                 </div>
-//                 <div classname="click-btn">
-//                     <button ><p><NavLink className="nav-link" to="/"><a>Finish</a></NavLink></p></button>
-//                 </div>
-//                 <div className="click-btn">
-//                     <p><NavLink className="nav-link" to="/signin"><a>Already have account?</a></NavLink></p>
-//                 </div>
-//
-//             </div>
-//         </div>
-//
-//     );
-// }
-//
-//
-// export default Signup
