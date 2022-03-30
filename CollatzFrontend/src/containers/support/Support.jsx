836e512fa20a04be31d382c298507a1729b5f946
qima53222 <1723162730@qq.com>
2022-03-29 19:56:42 -0600

    Demo pages improved

--- CollatzFrontend/src/containers/support/Support.jsx
+++ CollatzFrontend/src/containers/support/Support.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import './support.css';
+import header_logo from "../../assets/header_logo.png";
+import lowPolyEarth from "../../assets/lowPolyEarth.png";
+import {NavLink} from "react-router-dom";
 
 
 const Support = () => {
     return (
         <div className="Support">
             <div class="container">
-                <div class="row align-items-center my-5">
-                    <div class="col-lg-5">
-                        <h1 class="font-weight-light">Support Page</h1>
+                <div className="collatz__navbar-signin"><button>
+                    <NavLink to={"/"}>Back</NavLink>
+                </button>
+                </div>
+                <div className="collatz__navbar-links_logo">
+                    <br></br>
+                    <img src={header_logo} alt="header_logo"/>
+                </div>
+                <div>
+                        <h1 >Support Page</h1>
                         <p>
-                            This is support page demo
+                            <br></br>
+                            You can find support here:
                         </p>
-
-
-                    </div>
+                </div>
+                <div className="collatz-image">
+                    <img src={lowPolyEarth} alt="lowPolyEarth" />
                 </div>
             </div>
         </div>
--- CollatzFrontend/src/containers/whatCollatz/WhatCollatz.jsx
+++ CollatzFrontend/src/containers/whatCollatz/WhatCollatz.jsx
@@ -1,19 +1,44 @@
 import React from 'react';
 import './whatCollatz.css';
+import header_logo from "../../assets/header_logo.png";
+import lowPolyEarth from "../../assets/lowPolyEarth.png";
+import {NavLink} from "react-router-dom";
 
 const WhatCollatz = () => {
   return (
       <div className="WhatCollatz">
-          <div class="container">
-              <div class="row align-items-center my-5">
-                  <div class="col-lg-5">
-                      <h1 class="font-weight-light">WhatCollatz</h1>
-                      <p>
-                          WhatCollatz Introduction
-                          Demo
-                      </p>
+          <div className="container">
+                <div className="collatz__navbar-signin"><button>
+                    <NavLink to={"/"}>Back</NavLink>
+                </button>
+                </div>
+                  <div className="collatz__navbar-links_logo">
+                      <br></br>
+                      <img src={header_logo} alt="header_logo"/>
                   </div>
+                    <div>
+                      <h1 >What is Collatz</h1>
+                      <p>
+                          <br></br>
+                          Collatz Introduction <br></br>
+                          <br></br>
+                          Collatz is a website for users who are interested in the travel around the world.
+                          In this website, users can search the goal position with key words.
+                          For example, if users are interested in some specific scenic spots, such as mountains,
+                          they can fill in the corresponding keywords in the search bar.
+                          Our website can find the places associated with the keywords from the database.
+                          Back to the user interface, users can select the places they are interested in.
+                          The website also includes three categories: the name of the hotel in the target location,
+                          relevant flight information and rent. At the same time, the website can also search through the amount of budget,
+                          so that users can freely choose their favorite travel location without worrying about budget overruns.
+                          I hope users can have a perfect user experience in Collatz.
+                      </p>
+
+                  </div>
+              <div className="collatz-image">
+                  <img src={lowPolyEarth} alt="lowPolyEarth" />
               </div>
+
           </div>
       </div>
   );
