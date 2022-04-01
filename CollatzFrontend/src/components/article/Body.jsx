5d9e3ee0da72cee1b299279fedaf3ccbe8dac176
BWedeen <bewe0593@colorado.edu>
2022-03-31 20:44:53 -0600

    Report Structure

--- CollatzFrontend/src/components/article/Body.jsx
+++ CollatzFrontend/src/components/article/Body.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import './body.css';
 import {}  from './import';
 
-const Body = ( { imgUrl } ) => {
+const Body = ( { imgUrl, title, subtext } ) => {
   return (
     <div className="collatz__report-container_body">
       <div className="collatz__report-container_body-image">
         <img src={imgUrl} alt="body image" />
       </div>
         Body
+        <h2>
+          title
+        </h2>
+        <p>
+          subtext
+        </p>
     </div>
   )
 }
--- CollatzFrontend/src/containers/whatCollatz/WhatCollatz.jsx
+++ CollatzFrontend/src/containers/whatCollatz/WhatCollatz.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import './whatCollatz.css';
-import header_logo from "../../assets/header_logo.png";
 import { Navbar } from '../../components';
 import lowPolyEarth from "../../assets/lowPolyEarth.png";
-import {NavLink} from "react-router-dom";
 
 const WhatCollatz = () => {
   return (
@@ -17,7 +15,7 @@ const WhatCollatz = () => {
                     <h1 >What is Collatz</h1>
                     <p>
                         Collatz is a web app for planning travel on a set budget. 
-                        Collatz finds flights, hotels, attractions and transport at your travel destination within your set budget.
+                        Collatz finds flights, hotels, attractions and transport at your travel destination within your set budget. The front of this project is made with React.js and Python, using Google APIs.
                     </p>
                 </div>
             </div>
