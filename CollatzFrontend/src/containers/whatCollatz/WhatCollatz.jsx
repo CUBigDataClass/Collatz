ec3d9b18289b42915c94db3be90449fe959ce3c3
BWedeen <bewe0593@colorado.edu>
2022-04-02 20:03:34 -0600

    About Page
    - Added dynamic image
    - Fixed page length

--- CollatzFrontend/src/containers/whatCollatz/WhatCollatz.jsx
+++ CollatzFrontend/src/containers/whatCollatz/WhatCollatz.jsx
@@ -5,8 +5,8 @@ import lowPolyEarth from "../../assets/lowPolyEarth.png";
 
 const WhatCollatz = () => {
   return (
-    <div className="gradient__bg">
-        <div className="WhatCollatz">
+    <div className="WhatCollatz">
+        <div className="gradient__bg">
             <>
                 <Navbar/>
             </>
@@ -15,9 +15,12 @@ const WhatCollatz = () => {
                     <h1 >What is Collatz</h1>
                     <p>
                         Collatz is a web app for planning travel on a set budget. 
-                        Collatz finds flights, hotels, attractions and transport at your travel destination within your set budget. The front of this project is made with React.js and Python, using Google APIs.
+                        Collatz finds flights, hotels, attractions and transport at your travel destination within your set spending limit. The front end of this project is built with React.js and the back end is built in Python with the help of Google APIs.
                     </p>
                 </div>
+            </div>
+            <div className="collatz__about-image">
+                <img src={lowPolyEarth} alt="lowPolyEarth" />
             </div>
         </div>
     </div>
