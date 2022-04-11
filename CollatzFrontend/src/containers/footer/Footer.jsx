342c00b734c3100374df56255ccd5a602fd9be46
BWedeen <bewe0593@colorado.edu>
2022-04-11 14:52:20 -0600

    Report Decisions + Footer

--- CollatzFrontend/src/containers/footer/Footer.jsx
+++ CollatzFrontend/src/containers/footer/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './footer.css';
 
+import header_logo from '../../assets/header_logo.png';
+
 const Footer = () => {
   return (
-    <div>
-        
+    <div className='gradient__bg'>
+      <div className = "collatz__footer section__padding">
+        <div className='collatz__footer-logo'>
+          <img src={header_logo}/>
+        </div>
+      </div>
     </div>
   )
 }
