b7f9706999d24cc2957a1fab341890381fab6a86
BWedeen <bewe0593@colorado.edu>
2022-04-12 01:59:36 -0600

    Clean up

--- CollatzFrontend/src/components/index.js
+++ CollatzFrontend/src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Article } from './article/Article';
 export { default as Placeholder } from './placeholder/Placeholder';
-export { default as Feature } from './feature/Feature';
 export { default as Navbar } from './navbar/Navbar';
--- CollatzFrontend/src/containers/reportDecision/ReportDecision.jsx
+++ CollatzFrontend/src/containers/reportDecision/ReportDecision.jsx
@@ -10,7 +10,7 @@ const reportDecision = () => {
     <div className ="collatz__reportDecision">
         <div className="collatz__reportDecision-content">
             <p> Like this report? </p>
-            <h3> Save this report to your profile, or generate a new one.</h3>
+            <h3> Like it to save it to your profile, or generate a new one.</h3>
         </div>
         <div className="collatz__reportDecision-btn">
             <button type="button">  <FaCheck size="50px" /> </button>
