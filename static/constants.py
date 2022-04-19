d33f37fae7dc2819fcfaa0c7c710c92d480ca2c3
Sanjay G S <sanjaygs1997@gmail.com>
2022-04-19 16:47:37 -0600

    Create constants.py

---
+++ static/constants.py
@@ -0,0 +1,10 @@
+##Keys to search URL
+
+key = "774b07103bmsh7f4b47f00895eafp1ff206jsn58ad64733028"
+
+hotel_names_url = "https://priceline-com-provider.p.rapidapi.com/v1/hotels/locations"
+
+headers = {
+    'x-rapidapi-host': "priceline-com-provider.p.rapidapi.com",
+    'x-rapidapi-key': key
+    }
