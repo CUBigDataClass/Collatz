bb56814a40c566196b2cce96ca2a0a910f7e9ff1
Sanjay G S <sanjaygs1997@gmail.com>
2022-04-19 16:20:04 -0600

    Separating constants

---
+++ static/constants.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+key = "774b07103bmsh7f4b47f00895eafp1ff206jsn58ad64733028"
+
+hotel_names_url = "https://priceline-com-provider.p.rapidapi.com/v1/hotels/locations"
+
+headers = {
+    'x-rapidapi-host': "priceline-com-provider.p.rapidapi.com",
+    'x-rapidapi-key': key
+    }
+
+
+# In[3]:
+
+
+hotel_names_url
+
+
+# In[ ]:
+
+
+
+
