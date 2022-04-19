a1c6af8bdeb599bf5f108cb2d5243a4926a75a3a
Sushravya B C <52074361+sushravya-bc@users.noreply.github.com>
2022-04-19 16:40:35 -0600

    Create deploy-all.sh

---
+++ deploy-all.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+kubectl apply -f docker-compose.yaml
+# kubectl apply -f rest/rest-service.yaml
+
+# kubectl apply -f logs/logs-deployment.yaml
+
+# kubectl apply -f worker/worker-deployment.yaml
